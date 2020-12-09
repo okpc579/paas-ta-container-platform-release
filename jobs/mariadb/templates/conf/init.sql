25f4519ee347545b60fdd7b650bb7f043a45bb64
suslmk-lee <67575226+suslmk-lee@users.noreply.github.com>
2020-12-09 14:32:14 +0900

    commit

--- jobs/mariadb/templates/conf/init.sql
+++ jobs/mariadb/templates/conf/init.sql
@@ -58,6 +58,130 @@ CREATE TABLE `service_instance` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`cp` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `cp`;
+
+-- ----------------------------
+-- Table structure for admin_token
+-- ----------------------------
+DROP TABLE IF EXISTS `admin_token`;
+CREATE TABLE `admin_token`  (
+  `token_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'token name(토큰 명)',
+  `token_value` varchar(2000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'token value(토큰 값)',
+  PRIMARY KEY (`token_name`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '클러스터 어드민 토큰' ROW_FORMAT = Dynamic;
+
+
+-- ----------------------------
+-- Table structure for cp_clusters
+-- ----------------------------
+DROP TABLE IF EXISTS `cp_clusters`;
+CREATE TABLE `cp_clusters`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id(아이디)',
+  `cluster_api_url` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'cluster api url(클러스터 API URL)',
+  `cluster_name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'cluster name(클러스터 명)',
+  `cluster_token` varchar(2000) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'cluster token(클러스터 토큰)',
+  `created` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'created(생성일)',
+  `last_modified` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'last modified(수정일)',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '클러스터' ROW_FORMAT = Dynamic;
+
+
+-- ----------------------------
+-- Table structure for cp_limit_ranges
+-- ----------------------------
+DROP TABLE IF EXISTS `cp_limit_ranges`;
+CREATE TABLE `cp_limit_ranges`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id(아이디)',
+  `default_limit` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'default limit(기본 상한 값)',
+  `default_request` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'default request(기본 요청 값)',
+  `max` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'max(컨테이너에 지정할 리밋레인지 최대 값)',
+  `min` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'min(컨테이너에 지정할 리밋레인지 최소 값)',
+  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'name(리밋 레인지 명)',
+  `resource` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'resource(리밋 레인지 리소스)',
+  `type` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'type(리밋 레인지 유형)',
+  `created` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'created(생성일)',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '리밋 레인지' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of cp_limit_ranges
+-- ----------------------------
+INSERT INTO `cp_limit_ranges`(`id`, `default_limit`, `default_request`, `max`, `min`, `name`, `resource`, `type`, `created`) VALUES (1, '100m', '-', '-', '-', 'paas-ta-container-platform-low-limit-range', 'cpu', 'Container', '2020-11-05 13:26:24');
+INSERT INTO `cp_limit_ranges`(`id`, `default_limit`, `default_request`, `max`, `min`, `name`, `resource`, `type`, `created`) VALUES (2, '500Mi', '-', '-', '-', 'paas-ta-container-platform-low-limit-range', 'memory', 'Container', '2020-11-05 13:27:24');
+INSERT INTO `cp_limit_ranges`(`id`, `default_limit`, `default_request`, `max`, `min`, `name`, `resource`, `type`, `created`) VALUES (3, '300m', '-', '-', '-', 'paas-ta-container-platform-medium-limit-range', 'cpu', 'Container', '2020-11-05 13:27:59');
+INSERT INTO `cp_limit_ranges`(`id`, `default_limit`, `default_request`, `max`, `min`, `name`, `resource`, `type`, `created`) VALUES (4, '1500Mi', '-', '-', '-', 'paas-ta-container-platform-medium-limit-range', 'memory', 'Container', '2020-11-05 13:27:59');
+INSERT INTO `cp_limit_ranges`(`id`, `default_limit`, `default_request`, `max`, `min`, `name`, `resource`, `type`, `created`) VALUES (5, '900m', '-', '-', '-', 'paas-ta-container-platform-high-limit-range', 'cpu', 'Container', '2020-11-05 13:27:59');
+INSERT INTO `cp_limit_ranges`(`id`, `default_limit`, `default_request`, `max`, `min`, `name`, `resource`, `type`, `created`) VALUES (6, '3Gi', '-', '-', '-', 'paas-ta-container-platform-high-limit-range', 'memory', 'Container', '2020-11-05 13:27:59');
+
+-- ----------------------------
+-- Table structure for cp_resource_quotas
+-- ----------------------------
+DROP TABLE IF EXISTS `cp_resource_quotas`;
+CREATE TABLE `cp_resource_quotas`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id(아이디)',
+  `limit_cpu` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'limit cpu(cpu 상한 값)',
+  `limit_memory` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'limit memory(memory 상한 값)',
+  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'name(리소스 쿼터 명)',
+  `request_cpu` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'request cpu(cpu 요청 값)',
+  `request_memory` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'request memory(memory 요청 값)',
+  `status` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'status(할당량 및 현재 사용량 상태)',
+  `created` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'created(생성일)',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '리소스 쿼터' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of cp_resource_quotas
+-- ----------------------------
+INSERT INTO `cp_resource_quotas`(`id`, `limit_cpu`, `limit_memory`, `name`, `request_cpu`, `request_memory`, `status`, `created`) VALUES (1, '2', '2Gi', 'paas-ta-container-platform-low-rq', '-', '-', '{\"cpu\":{\"used\":\"200m\",\"hard\":\"2\"}, \"memory\":{\"used\":\"1800Mi\",\"hard\": \"2Gi\"}}', '2020-11-05 13:26:24');
+INSERT INTO `cp_resource_quotas`(`id`, `limit_cpu`, `limit_memory`, `name`, `request_cpu`, `request_memory`, `status`, `created`) VALUES (2, '4', '6Gi', 'paas-ta-container-platform-medium-rq', '-', '-', '{\"cpu\":{\"used\":\"200m\",\"hard\":\"4\"}, \"memory\":{\"used\":\"1800Mi\",\"hard\": \"6Gi\"}}', '2020-11-05 13:26:24');
+INSERT INTO `cp_resource_quotas`(`id`, `limit_cpu`, `limit_memory`, `name`, `request_cpu`, `request_memory`, `status`, `created`) VALUES (3, '8', '12Gi', 'paas-ta-container-platform-high-rq', '-', '-', '{\"cpu\":{\"used\":\"200m\",\"hard\":\"8\"}, \"memory\":{\"used\":\"1800Mi\",\"hard\": \"12Gi\"}}', '2020-11-05 13:26:24');
+
+-- ----------------------------
+-- Table structure for cp_users
+-- ----------------------------
+DROP TABLE IF EXISTS `cp_users`;
+CREATE TABLE `cp_users`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id(아이디)',
+  `cluster_api_url` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'cluster api url(클러스터 API URL)',
+  `cluster_name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'cluster name(클러스터 명)',
+  `cluster_token` varchar(2000) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'cluster token(클러스터 토큰)',
+  `created` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'created(생성일)',
+  `description` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'description(설명)',
+  `email` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'email(이메일)',
+  `last_modified` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'last modified(수정일)',
+  `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'password(비밀번호)',
+  `role_set_code` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'role set code(권한)',
+  `service_account_name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'service account name(service account 명)',
+  `user_id` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'user id(사용자 아이디)',
+  `user_type` varchar(32) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'user type(사용자 유형)',
+  `namespace` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'namespace(namespace 명)',
+  `is_active` varchar(1) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'N' COMMENT 'is active(사용 여부)',
+  `service_account_secret` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'service account secret(service account secret 명)',
+  `service_account_token` varchar(2000) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT 'service account token(service account 토큰)',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1178 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci COMMENT = '사용자' ROW_FORMAT = Dynamic;
+
+INSERT INTO `cp`.`admin_token`(`token_name`, `token_value`) VALUES ('caas_admin', '<%= p("k8s_auth_bearer") %>');
+INSERT INTO `broker`.`admin_token`(`token_name`, `token_value`) VALUES ('caas_admin', '<%= p("k8s_auth_bearer") %>');
+
+-- ----------------------------
+-- Table structure for private_repository
+-- ----------------------------
+DROP TABLE IF EXISTS `private_repository`;
+CREATE TABLE `private_repository`  (
+  `seq` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'seq(시퀀스)',
+  `repository_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'repository url(저장소 URL)',
+  `repository_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'repository name(저장소 명)',
+  `image_name` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'image name(이미지명)',
+  `image_version` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'image version(이미지버전)',
+  PRIMARY KEY (`seq`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '프라이빗 레파지토리' ROW_FORMAT = Dynamic;
+
+INSERT INTO `cp`.`private_repository`(`repository_url`, `image_name`, `image_version`) VALUES ('<%= link("private-image-repository-link").p("image_repository.registry.url") %>', '<%= link("private-image-repository-link").p("image_repository.registry.name") %>','<%= link("private-image-repository-link").p("image_repository.registry.version") %>');
+
+
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`caas` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 USE `caas`;
@@ -67,9 +191,11 @@ DROP TABLE IF EXISTS `admin_token`;
 
 CREATE TABLE `admin_token` (
   `token_name` varchar(255) NOT NULL,
-  `token_value` varchar(1200) NOT NULL,
+  `token_value` varchar(1000) NOT NULL,
   PRIMARY KEY (`token_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+INSERT INTO `caas`.`admin_token`(`token_name`, `token_value`) VALUES ('caas_admin', '<%= p("k8s_auth_bearer") %>');
 
 /*Table structure for table `user` */
 DROP TABLE IF EXISTS `user`;
@@ -95,11 +221,11 @@ CREATE TABLE `user` (
 /*Table structure for table `caas_user_role_set` */
 DROP TABLE IF EXISTS `caas_user_role_set`;
 
-CREATE TABLE `caas_user_role_set` ( 
+CREATE TABLE `caas_user_role_set` (
   `role_set_code` char(6) NOT NULL,
   `resource_code` varchar(20) NOT NULL,
   `verb_code` varchar(20) NOT NULL,
-  `description` varchar(400) DEFAULT NULL, 
+  `description` varchar(400) DEFAULT NULL,
   `created` varchar(20) DEFAULT NULL,
   PRIMARY KEY (role_set_code, resource_code, verb_code)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -117,8 +243,6 @@ INSERT INTO `caas`.`caas_user_role_set`(`role_set_code`, `resource_code`, `verb_
 INSERT INTO `caas`.`caas_user_role_set`(`role_set_code`, `resource_code`, `verb_code`, `description`, `created`) VALUES ('<%= p("role_set.regular_user_code") %>', 'user_management', 'list', '', now());
 INSERT INTO `caas`.`caas_user_role_set`(`role_set_code`, `resource_code`, `verb_code`, `description`, `created`) VALUES ('<%= p("role_set.regular_user_code") %>', 'user_management', 'watch', '', now());
 
-INSERT INTO `caas`.`caas_user_role_set`(`role_set_code`, `resource_code`, `verb_code`, `description`, `created`) VALUES ('<%= p("role_set.init_user_code") %>', 'user_management', 'get', '', now());
-INSERT INTO `caas`.`caas_user_role_set`(`role_set_code`, `resource_code`, `verb_code`, `description`, `created`) VALUES ('<%= p("role_set.init_user_code") %>', 'user_management', 'list', '', now());
-INSERT INTO `caas`.`caas_user_role_set`(`role_set_code`, `resource_code`, `verb_code`, `description`, `created`) VALUES ('<%= p("role_set.init_user_code") %>', 'user_management', 'watch', '', now());
-INSERT INTO `caas`.`admin_token`(`token_name`, `token_value`) VALUES ('caas_admin', '<%= link("caas-jenkins-broker-link").p("caas.k8s_auth_bearer") %>');
-INSERT INTO `broker`.`admin_token`(`token_name`, `token_value`) VALUES ('caas_admin', '<%= link("caas-jenkins-broker-link").p("caas.k8s_auth_bearer") %>');
+
+SET FOREIGN_KEY_CHECKS = 1;
+
